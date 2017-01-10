9d6a10d3d63bef611238d88e12de78d9248cf89f
aiborisov <a.borisov@mobile.agima.ru>
2017-01-10 21:55:35 +0400

    Initial Commit

---
+++ IBIZI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  IBIZI
+//
+//  Created by alex borisoft on 10.01.17.
+//  Copyright © 2017 robolife. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
